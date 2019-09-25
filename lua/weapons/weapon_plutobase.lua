b1614aa2f52eb3fd9d104d347893dafb48334e0c
Meepen <meepdarknessmeep@gmail.com>
2019-09-24 22:55:51 -0400

    Add DamageOverride

--- lua/weapons/weapon_plutobase.lua
+++ lua/weapons/weapon_plutobase.lua
@@ -4,5 +4,14 @@ DEFINE_BASECLASS "weapon_tttbase_old"
 function SWEP:SetupDataTables()
 	BaseClass.SetupDataTables(self)
 
-	print "gay"
+	self:NetVar("DamageMod", "Float", 1)
+end
+
+function SWEP:Initialize()
+	self.GetDamage_Old = self.GetDamage
+	self.GetDamage = self.GetDamageOverride
+end
+
+function SWEP:GetDamageOverride()
+	return self:GetDamageMod() * self:GetDamage_Old()
 end
