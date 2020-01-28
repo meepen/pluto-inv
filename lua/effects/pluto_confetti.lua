a693ded72fcb4203fce482f0b7e194ba3e759008
Meepen <meepdarknessmeep@gmail.com>
2020-01-27 22:26:04 -0500

    fix confetti to make it work as a tracer

--- lua/effects/pluto_confetti.lua
+++ lua/effects/pluto_confetti.lua
@@ -1,12 +1,14 @@
 local sprite = "particles/balloon_bit"
-local spread = 50
-local count = 50
+local spread = 70
+local count = 100
 
 function EFFECT:Init(data)
-    local pos = data:GetOrigin()
-    local dir = data:GetNormal()
+    local pos = data:GetStart()
+    local dir = data:GetOrigin() - pos
+    dir:Normalize()
     local emitter = ParticleEmitter(pos, false)
     if not IsValid(emitter) then return end
+    
     local endPoint = Vector(pos + dir * 100)
 
     for i = 1, count do
@@ -14,19 +16,19 @@ function EFFECT:Init(data)
 
         if particle then
             particle:SetLifeTime(0)
-            particle:SetDieTime(1)
+            particle:SetDieTime(4)
             particle:SetStartAlpha(255)
             particle:SetEndAlpha(50)
             particle:SetStartSize(2)
-            particle:SetEndSize(2)
+            particle:SetEndSize(0)
             particle:SetCollide(false)
             particle:SetRoll(math.Rand(0, 360))
             particle:SetRollDelta(math.Rand(-2, 2))
             particle:SetAirResistance(100)
-            particle:SetGravity(Vector(0, 0, -5))
+            particle:SetGravity(Vector(0, 0, -222))
             local colour = HSVToColor(math.random(0, 360), 1, 1)
             particle:SetColor(colour.r, colour.g, colour.b)
-            particle:SetLighting(true)
+            particle:SetLighting(false)
             particle:SetCollide(true)
             particle:SetVelocity(endPoint + Vector(math.random(-spread, spread), math.random(-spread, spread), math.random(-spread, spread)) - pos)
         end
