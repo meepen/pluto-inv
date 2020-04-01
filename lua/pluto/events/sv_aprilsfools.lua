df08f02cc28daf4d64c25ae5f4df6920f48ad5ce
Meepen <meepdarknessmeep@gmail.com>
2020-04-01 06:01:36 -0400

    :april fools part 1

---
+++ lua/pluto/events/sv_aprilsfools.lua
@@ -0,0 +1,10 @@
+hook.Add("TTTBeginRound", "pluto_april_fools", function()
+	if (math.random() > 0.95) then
+		hook.Remove("TTTBeginRound", "pluto_april_fools")
+		for _, ply in pairs(player.GetAll()) do
+			for bone = 0, ply:GetBoneCount() - 1 do
+				ply:ManipulateBoneJiggle(bone, 1)
+			end
+		end
+	end
+end)
