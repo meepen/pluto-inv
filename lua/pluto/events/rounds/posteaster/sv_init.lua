369e74fd2e69a399befa88b0835d63c4c34778b6
Meepen <meepdarknessmeep@gmail.com>
2020-04-25 11:09:57 -0400

    change boss count to be ceil instead of floor

--- lua/pluto/events/rounds/posteaster/sv_init.lua
+++ lua/pluto/events/rounds/posteaster/sv_init.lua
@@ -54,7 +54,7 @@ ROUND:Hook("TTTSelectRoles", function(self, state, plys)
 	plys = table.shuffle(plys)
 
 	local roles_needed = {
-		Child = math.max(1, math.floor(#plys / 4)),
+		Child = math.max(1, math.ceil(#plys / 4)),
 	}
 
 	for i, ply in ipairs(plys) do
