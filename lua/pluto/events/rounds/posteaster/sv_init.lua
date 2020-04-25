0479b009742f0c01870225fc0d0f524a12897f7c
Meepen <meepdarknessmeep@gmail.com>
2020-04-25 11:09:29 -0400

    add navmesh walking to find walking distances suitable for spawnpoints for boss

--- lua/pluto/events/rounds/posteaster/sv_init.lua
+++ lua/pluto/events/rounds/posteaster/sv_init.lua
@@ -4,6 +4,11 @@ ROUND.EggSpread = 150
 ROUND.BunnyLives = 3
 ROUND.CollisionGroup = COLLISION_GROUP_DEBRIS_TRIGGER
 
+ROUND.BunnySpawnDistances = {
+	Min = 8000,
+	Max = 10000
+}
+
 ROUND.Boss = true
 
 local badasses = {
@@ -28,7 +33,7 @@ ROUND.RoundDatas = {
 	},
 	{
 		BunnyModel = {"dom_rabbit", "wild_rabbit"},
-		Health = 50,
+		Health = 30,
 		ChildModel = {"male_child", "female_child"},
 		Shares = 15,
 	}
@@ -51,9 +56,6 @@ ROUND:Hook("TTTSelectRoles", function(self, state, plys)
 	local roles_needed = {
 		Child = math.max(1, math.floor(#plys / 4)),
 	}
-
-	PrintTable(plys)
-	PrintTable(roles_needed)
 
 	for i, ply in ipairs(plys) do
 		local role, amt = next(roles_needed)
@@ -100,6 +102,84 @@ local function GetModel(model)
 	return pluto.models[model].Model
 end
 
+function ROUND:BunnySpawn(ply, state)
+	local clusters = {}
+
+	for _, cluster in pairs(state.clusters) do
+		if (IsValid(cluster.Player) and cluster.Player:Alive()) then
+			table.insert(clusters, cluster)
+		end
+	end
+
+	for i = 1, 10 do
+		for _, cluster in RandomPairs(clusters) do
+			local valid = false
+			for _, currency in pairs(cluster.Currencies) do
+				if (IsValid(currency)) then
+					valid = true
+					break
+				end
+			end
+
+			if (not valid) then
+				continue
+			end
+			for _, nav in RandomPairs(cluster.SpawnNavs) do
+				local pos = pluto.currency.validpos(nav.Nav)
+
+				if (not pos) then
+					continue
+				end
+
+				local dist = nav.Distance + nav.LastPos:Distance(pos)
+
+				if (dist <= self.BunnySpawnDistances.Max and dist >= self.BunnySpawnDistances.Min) then
+					return pos
+				end
+			end
+		end
+	end
+end
+
+function ROUND:ProcessNav(output, nav, max_distance, target, cur_distance)
+	if (not output.processed) then
+		output.processed = {}
+	end
+
+	if (not cur_distance) then
+		cur_distance = 0
+	end
+
+	for _, area in pairs(nav:GetAdjacentAreas()) do
+		local closest = area:GetClosestPointOnArea(target)
+		local distance = closest:Distance(target) + cur_distance
+
+		local processed = output.processed[area:GetID()]
+
+		if (processed and processed.Distance < distance) then
+			continue
+		end
+
+		if (distance < max_distance) then
+			local t = processed or {}
+
+			t.Nav = nav
+			t.Distance = distance
+			t.LastPos = closest
+
+			output.processed[area:GetID()] = t
+			table.insert(output, t)
+			self:ProcessNav(output, area, max_distance, target, distance)
+		end
+	end
+end
+
+function ROUND:ProcessNavAreasNear(output, nav, max_distance, target, cur_distance)
+	self:ProcessNav(output, nav, max_distance, target, cur_distance)
+
+	output.processed = nil
+end
+
 ROUND:Hook("TTTBeginRound", function(self, state)
 	-- remove doors gae
 	for _, ent in pairs(ents.GetAll()) do
@@ -113,18 +193,6 @@ ROUND:Hook("TTTBeginRound", function(self, state)
 	state.lives = {}
 	local bunnies = round.GetActivePlayersByRole "Bunny"
 
-	for _, ply in pairs(children) do
-		ply:SetModel(GetModel(state.Data.ChildModel))
-		ply:SetCollisionGroup(self.CollisionGroup)
-	end
-	for _, ply in pairs(bunnies) do
-		ply:SetModel(GetModel(state.Data.BunnyModel))
-		ply:SetHealth(state.Data.Health)
-		ply:SetMaxHealth(state.Data.Health)
-		state.lives[ply] = self.BunnyLives
-		ply:SetCollisionGroup(self.CollisionGroup)
-	end
-
 	state.clusters = {}
 
 	local tries = 1000
@@ -136,9 +204,12 @@ ROUND:Hook("TTTBeginRound", function(self, state)
 		if (random) then
 			local cluster = {random, Player = children[1]}
 
-			local navs = pluto.currency.navs_filter(function(nav)
-				return nav:GetClosestPointOnArea(random):Distance(random) < self.EggSpread
-			end)
+			local navs = {}
+			self:ProcessNavAreasNear(navs, nav, self.EggSpread, random)
+			
+			-- generate navmeshes close to this
+			cluster.SpawnNavs = {}
+			self:ProcessNavAreasNear(cluster.SpawnNavs, nav, self.BunnySpawnDistances.Max, random)
 
 			local spawn_location_tries = 5 * self.EggsPerCluster
 			while (#cluster < self.EggsPerCluster) do
@@ -147,8 +218,8 @@ ROUND:Hook("TTTBeginRound", function(self, state)
 					break
 				end
 
-				for k, nav in pairs(navs) do
-					local newpos = pluto.currency.validpos(nav)
+				for k, nav in RandomPairs(navs) do
+					local newpos = pluto.currency.validpos(nav.Nav)
 					if (not newpos) then
 						continue
 					end
@@ -171,6 +242,9 @@ ROUND:Hook("TTTBeginRound", function(self, state)
 			if (spawn_location_tries > 0) then
 				table.insert(state.clusters, cluster)
 				table.remove(children, 1)
+				if (cluster.Player:Alive()) then
+					self:Initialize(state, cluster.Player)
+				end
 			end
 		end
 	end
@@ -180,19 +254,56 @@ ROUND:Hook("TTTBeginRound", function(self, state)
 	end
 
 	for _, cluster in pairs(state.clusters) do
-		timer.Simple(0, function()
-			if (IsValid(cluster.Player)) then
-				cluster.Player:SetPos(cluster[1])
-			end
-		end)
-
 		cluster.Currencies = {}
 
 		for i, pos in ipairs(cluster) do
 			cluster.Currencies[i] = pluto.currency.spawnfor(cluster.Player, "crate3_n", pos, true)
 		end
 	end
+
+	for _, ply in pairs(bunnies) do
+		state.lives[ply] = self.BunnyLives
+		if (ply:Alive()) then
+			self:Initialize(state, ply)
+		end
+	end
 end)
+
+function ROUND:Initialize(state, ply)
+	self:PlayerSetModel(state, ply)
+	self:Spawn(state, ply)
+	local pos = self:ResetPosition(state, ply)
+	if (pos) then
+		ply:SetPos(pos)
+	end
+end
+
+function ROUND:Spawn(state, ply)
+	ply:SetCollisionGroup(self.CollisionGroup)
+	if (state.lives and state.lives[ply]) then
+		state.lives[ply] = state.lives[ply] - 1
+		ply:SetHealth(state.Data.Health)
+		ply:SetMaxHealth(state.Data.Health)
+	end
+end
+
+ROUND:Hook("PlayerSpawn", ROUND.Spawn)
+
+function ROUND:ResetPosition(state, ply)
+	if (not state.clusters) then
+		return
+	end
+
+	for _, cluster in pairs(state.clusters) do
+		if (cluster.Player == ply) then
+			return cluster[1]
+		end
+	end
+
+	return self:BunnySpawn(ply, state)
+end
+
+ROUND:Hook("PlayerSelectSpawnPosition", ROUND.ResetPosition)
 
 ROUND:Hook("TTTEndRound", function(self, state)
 	local children = state.children
@@ -233,10 +344,6 @@ ROUND:Hook("PostPlayerDeath", function(self, state, ply)
 		timer.Simple(1, function()
 			if (IsValid(ply)) then
 				ttt.ForcePlayerSpawn(ply)
-				state.lives[ply] = state.lives[ply] - 1
-				ply:SetHealth(state.Data.Health)
-				ply:SetMaxHealth(state.Data.Health)
-				ply:SetCollisionGroup(self.CollisionGroup)
 			end
 		end)
 	else
