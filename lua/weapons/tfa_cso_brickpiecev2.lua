4bac3e52f46be2de12048b9a66da34a69787d662
Meepen <meepdarknessmeep@gmail.com>
2020-04-29 17:25:31 -0400

    finish a unique

--- lua/weapons/tfa_cso_brickpiecev2.lua
+++ lua/weapons/tfa_cso_brickpiecev2.lua
@@ -64,33 +64,71 @@ SWEP.DamageSounds = {
 	}
 }
 
-function SWEP:FireBulletsCallback(tr, dmginfo, data)
-	local r = BaseClass.FireBulletsCallback(self, tr, dmginfo, data)
+function SWEP:DoFireBullets(src, dir, data)
+	if (CLIENT) then
+		return
+	end
 
-	if (SERVER and IsValid(hit) and hit:IsPlayer() and dmginfo:GetDamage() > 0 and math.random() > 0.5) then
-		local hg = tr.HitGroup
-		local hit = tr.Entity
+	local owner = self:GetOwner()
 
-		local fmt = "vo/npc/female01/%s.wav"
+	local e = ents.Create "prop_physics"
+	e:SetModel "models/hunter/plates/plate.mdl"
 
-		if (pluto.models.gendered[hit:GetModel()] == "Male") then
-			fmt = "vo/npc/male01/%s.wav"
+	dir = dir or owner:EyeAngles():Forward()
+	src = src or owner:GetShootPos()
+
+	e:SetModelScale(0.5 + math.random() * 0.5, 0)
+	e:SetPos(src + dir * 50)
+	e:Spawn()
+	e:GetPhysicsObject():SetVelocity(dir * 10000)
+	e:GetPhysicsObject():SetMass(5)
+
+	e:AddCallback("PhysicsCollide", function(self, dat)
+		local other = dat.HitEntity
+		if (IsValid(other) and other:IsPlayer()) then
+			timer.Simple(0, function()
+				if (IsValid(self)) then
+					self:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
+				end
+			end)
 		end
+	end)
+	e.Weapon = self
+	e.Owner = self:GetOwner()
 
-		local type = "default"
+	hook.Add("EntityTakeDamage", e, function(self, vic, dmg)
+		if (vic:IsPlayer() and dmg:GetInflictor() == self) then
+			dmg:SetDamage(self.Weapon.Primary.Damage)
+			dmg:SetInflictor(self.Weapon)
+			dmg:SetAttacker(self.Owner)
 
-		if (math.random() > 0.75) then
-			if (hg == HITGROUP_LEFTLEG or hg == HITGROUP_RIGHTLEG) then
-				type = "leg"
-			elseif (hg == HITGROUP_CHEST or hg == HITGROUP_STOMACH or hg == HITGROUP_GEAR) then
-				type = "chest"
+
+			if (math.random() > 0.3) then
+				local hg = -1
+
+				local fmt = pluto.models.gendered[vic:GetModel()] == "Male" and "vo/npc/male01/%s.wav" or "vo/npc/female01/%s.wav"
+
+				local type = "default"
+
+				if (math.random() > 0.25) then
+					if (hg == HITGROUP_RIGHTLEG or hg == HITGROUP_LEFTLEG) then
+						type = "leg"
+					elseif (hg == HITGROUP_CHEST or hg == HITGROUP_GEAR or hg == HITGROUP_STOMACH) then
+						type = "chest"
+					end
+				end
+
+				vic:EmitSound(string.format(fmt, (table.Random(self.Weapon.DamageSounds[type]))))
+				self:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
 			end
 		end
+	end)
 
-		local voiceline = table.Random(self.DamageSounds[type])
+	e:SetColor(ColorRand())
 
-		hit:EmitSound(string.format(fmt, voiceline), 75, 100, 1, CHAN_USER_BASE + 6)
-	end
-	
-	return r
+	timer.Simple(10, function()
+		if (IsValid(e)) then
+			e:Remove()
+		end
+	end)
 end
