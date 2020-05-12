855a27c4bb3ec0accef761f0f8c996e8ffd591a1
Meepen <meepdarknessmeep@gmail.com>
2020-05-12 12:58:41 -0400

    add fake entity for rope to latch to instead of player for nitro to fix bugs

--- lua/weapons/weapon_nitro_rope.lua
+++ lua/weapons/weapon_nitro_rope.lua
@@ -12,7 +12,48 @@ function SWEP:Initialize()
 	BaseClass.Initialize(self)
 
 	self.Ropes = {}
+
+	if (not SERVER) then
+		return
+	end
+
+	self.FakePlayer = ents.Create "prop_physics"
+	if (IsValid(self.FakePlayer)) then
+		self.FakePlayer:SetPos(self:GetPos())
+
+		self.FakePlayer:SetModel("models/weapons/w_bugbait.mdl")
+
+		self.FakePlayer:SetColor(Color(50, 250, 50, 240))
+		self.FakePlayer:SetNoDraw(true)
+		self.FakePlayer:DrawShadow(false)
+
+		self.FakePlayer:SetHealth(999)
+		self.FakePlayer:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+		self.FakePlayer:SetSolid(SOLID_NONE)
+
+		self.FakePlayer:Spawn()
+		local phys = self.FakePlayer:GetPhysicsObject()
+		if IsValid(phys) then
+			phys:SetMass(200)
+			phys:SetDamping(0, 1000)
+			phys:EnableGravity(false)
+			phys:EnableCollisions(false)
+			phys:EnableMotion(false)
+			phys:AddGameFlag(FVPHYSICS_PLAYER_HELD)
+		end
+	end
 end
+
+function SWEP:AllowTether(target)
+	local phys = target:GetPhysicsObject()
+	local ply = self:GetOwner()
+
+	return IsValid(phys) and IsValid(ply) and
+			  (not phys:HasGameFlag(FVPHYSICS_NO_PLAYER_PICKUP)) and
+			  phys:GetMass() < 150 and
+			  (target.CanPickup != false)
+end
+
 
 function SWEP:PrimaryAttack()
 	if (IsValid(self.EntHolding)) then
@@ -45,13 +86,13 @@ function SWEP:PrimaryAttack()
 	}
 	owner:LagCompensation(false)
 
-	if (not IsValid(tr.Entity)) then
+	if (not IsValid(tr.Entity) or not self:AllowTether(tr.Entity)) then
 		return
 	end
 
 
-	local rope = constraint.Rope(owner, tr.Entity, 0, 0, vector_origin, tr.Entity:WorldToLocal(tr.HitPos),
-		length, 20, 1300,
+	local rope = constraint.Rope(self.FakePlayer, tr.Entity, 0, 0, vector_origin, tr.Entity:WorldToLocal(tr.HitPos),
+		length, length * 0.1, 2500,
 		1, "cable/xbeam", false)
 
 	if (not IsValid(rope)) then
@@ -68,6 +109,11 @@ function SWEP:Think()
 
 	if (not SERVER) then
 		return
+	end
+
+	if (IsValid(self:GetOwner())) then
+		self.FakePlayer:SetPos(self:GetOwner():GetPos() + vector_up * 20)
+		self.FakePlayer:SetAngles(self:GetOwner():GetAngles())
 	end
 
 	for rope, attached in pairs(self.Ropes) do
@@ -92,6 +138,10 @@ function SWEP:OnRemove()
 		if (IsValid(rope)) then
 			rope:Remove()
 		end
+	end
+
+	if (IsValid(self.FakePlayer)) then
+		self.FakePlayer:Remove()
 	end
 
 	return BaseClass.OnRemove(self)
