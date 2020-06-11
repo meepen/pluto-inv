b8aabf7e7112b2e27e642ed6c131295c902eaef9
Meepen <meepdarknessmeep@gmail.com>
2020-06-11 16:04:42 -0400

    add new suffix

---
+++ lua/pluto/mods/modifiers/leech.lua
@@ -0,0 +1,33 @@
+MOD.Type = "suffix"
+MOD.Name = "The Leech"
+MOD.Tags = {
+	"damage", "heal"
+}
+
+function MOD:IsNegative(roll)
+	return roll < 0
+end
+
+function MOD:FormatModifier(index, roll)
+	return string.format("%.01f%%", roll)
+end
+
+MOD.Description = "Damage is lowered by %s. Heal for %s of damage."
+
+MOD.Tiers = {
+	{ 8, 15, 25, 35 },
+	{ 8, 15, 15, 25 },
+	{ 8, 15, 10,  15 },
+}
+
+function MOD:PreDamage(wep, rolls, vic, dmginfo, state)
+	if (IsValid(vic) and vic:IsPlayer() and dmginfo:GetDamage() > 0) then
+		dmginfo:ScaleDamage(1 - rolls[1] / 100)
+		local atk = wep:GetOwner()
+		if (IsValid(atk)and atk:IsPlayer() and atk:Alive()) then
+			atk:SetHealth(math.min(atk:GetMaxHealth(), atk:Health() + dmginfo:GetDamage() * rolls[2] / 100))
+		end
+	end
+end
+
+return MOD
--- lua/pluto/mods/sv_mods.lua
+++ lua/pluto/mods/sv_mods.lua
@@ -24,6 +24,7 @@ for _, filename in pairs {
 	"greed",
 	"handed",
 	"hearted",
+	"leech",
 	"limp",
 	"mag",
 	"max_range",
