ac7caaec4e527c216e59f349e30f6e27ce62c076
Meepen <meepdarknessmeep@gmail.com>
2020-11-09 13:48:57 -0500

    more stuff

--- lua/pluto/inv/ui.lua
+++ lua/pluto/inv/ui.lua
@@ -483,8 +483,135 @@ function PANEL:OnCursorExited()
 	end
 end
 
+local function CreateMenu(self, item)
+	local rightclick_menu = DermaMenu()
+	if (self.Tab and self.Tab.Type ~= "equip") then
+		rightclick_menu:AddOption("Equip",function()
+			local thistab = pluto.tabs[self.Tab.Type]
+
+			for i = 1, tabtype.size do
+				if (tabtype.canaccept(i, self.Item) and (not t.Items[self.TabIndex] or thistab.canaccept(self.TabIndex, t.Items[self.TabIndex]))) then
+					self:SwitchWith(tabele.Items[i])
+				end
+			end
+		end):SetIcon("icon16/add.png")
+	end
+
+	rightclick_menu:AddOption("Upload item stats", function()
+		local StatsRT = GetRenderTarget("ItemStatsRT" .. ScrW() .. "_" ..  ScrH(), ScrW(), ScrH())
+		OVERRIDE_DETAILED = true
+		local show = pluto.ui.showcase(self.Item)
+		pluto.ui.showcasepnl = nil
+		show:SetPaintedManually(true)
+		local item_name = self.Item:GetPrintName()
+		timer.Simple(0, function()
+			hook.Add("PostRender", "Upload", function()
+				hook.Remove("PostRender", "Upload")
+				OVERRIDE_DETAILED = false
+				cam.Start2D()
+				render.PushRenderTarget(StatsRT)
+				if (not pcall(show.PaintManual, show)) then
+					Derma_Message("Encountered an error while generating the image! Please try again.", "Upload failed", "Thanks")
+
+					render.Clear(0, 0, 0, 0)
+					render.PopRenderTarget(StatsRT)
+					cam.End2D()
+					show:Remove()
+				return end
+				local data = render.Capture {
+					format = "png",
+					quality = 100,
+					h = show:GetTall(),
+					w = show:GetWide(),
+					x = 0,
+					y = 0,
+					alpha = false,
+				}
+				render.Clear(0, 0, 0, 0)
+				render.PopRenderTarget(StatsRT)
+				cam.End2D()
+				show:Remove()
+				HTTP {
+					url = "https://api.imgur.com/3/album",
+					method = "post",
+					headers = {
+						Authorization = "Client-ID 3693fd6ea039830",
+					},
+					success = function(_, c, _, _)
+						local album = util.JSONToTable(c)
+						if (not album.success) then
+							Derma_Message("Your upload was not successful! Please try again.", "Upload failed", ":(")
+							return
+						end
+
+						HTTP {
+							url = "https://api.imgur.com/3/image",
+							method = "post",
+							headers = {
+								Authorization = "Client-ID 3693fd6ea039830"
+							},
+							success = function(_, b, _, _)
+								b = util.JSONToTable(b)
+								if (b.success) then
+									Derma_Message("Your picture of your stats has been uploaded and copied to your clipboard!\nYou can now simply paste it anywhere, like our Discord! discord.gg/pluto", "Upload success", "Thanks!")
+									SetClipboardText("https://imgur.com/a/" .. album.data.id)
+								else
+									Derma_Message("Your upload was not successful! Please try again.", "Upload failed", "Thanks")
+								end
+							end,
+							failed = function(a) 
+								Derma_Message("Imgur appears to be having some issues, please wait and try again!", "Upload failed", "Ok")
+							end,
+							parameters = {
+								image = util.Base64Encode(data),
+								album = album.data.deletehash
+							},
+						}
+					end,
+					failed = function(a)
+						Derma_Message("Your upload was not successful! Please try again.", "Upload failed", "Ok")
+					end,
+					parameters = {
+						title = string.format("%s's %s", LocalPlayer():Nick(), item_name),
+						description = string.format("Taken by %s https://steamcommunity.com/profiles/%s\nDiscord: https://discord.gg/pluto\nWebsite: https://pluto.gg\n\n%s", LocalPlayer():Nick(), LocalPlayer():SteamID64(), self.Item:GetTextMessage()), 
+					},
+				}
+			end)
+		end)
+	end):SetIcon("icon16/camera.png")
+
+	if (self.Item.Type ~= "Shard") then
+		rightclick_menu:AddOption("Toggle locked", function()
+			pluto.inv.message()
+				:write("itemlock", self.Item.ID)
+				:send()
+		end):SetIcon("icon16/lock.png")
+	end
+
+	if (not self.Item.Locked and self.Item.Nickname) then
+		rightclick_menu:AddOption("Remove name (100 hands)", function()
+			self.Item.Nickname = nil
+			pluto.inv.message()
+				:write("unname", self.Item.ID)
+				:send()
+		end):SetIcon("icon16/cog_delete.png")
+	end
+
+	if (LocalPlayer():GetUserGroup() == "developer") then
+		rightclick_menu:AddOption("Duplicate", function()
+			RunConsoleCommand("pluto_item_dupe", self.Item.ID)
+		end):SetIcon("icon16/cog_delete.png")
+	end
+
+	rightclick_menu:Open()
+	rightclick_menu:SetPos(input.GetCursorPos())--s
+end
+
 function PANEL:OnMousePressed(code)
 	if (self.NoMove) then
+		if (code == MOUSE_RIGHT and LocalPlayer():GetUserGroup() == "developer") then
+			CreateMenu(self, self.Item)
+		end
 		return
 	end
 
@@ -511,127 +638,8 @@ function PANEL:OnMousePressed(code)
 			if (not IsValid(tabele) or not tabtype) then
 				return
 			end
-			local rightclick_menu = DermaMenu()
-			if (self.Tab.Type ~= "equip") then
-				rightclick_menu:AddOption("Equip",function()
-					local thistab = pluto.tabs[self.Tab.Type]
 
-					for i = 1, tabtype.size do
-						if (tabtype.canaccept(i, self.Item) and (not t.Items[self.TabIndex] or thistab.canaccept(self.TabIndex, t.Items[self.TabIndex]))) then
-							self:SwitchWith(tabele.Items[i])
-						end
-					end
-				end):SetIcon("icon16/add.png")
-			end
-
-			rightclick_menu:AddOption("Upload item stats", function()
-				local StatsRT = GetRenderTarget("ItemStatsRT" .. ScrW() .. "_" ..  ScrH(), ScrW(), ScrH())
-				OVERRIDE_DETAILED = true
-				local show = pluto.ui.showcase(self.Item)
-				pluto.ui.showcasepnl = nil
-				show:SetPaintedManually(true)
-				local item_name = self.Item:GetPrintName()
-				timer.Simple(0, function()
-					hook.Add("PostRender", "Upload", function()
-						hook.Remove("PostRender", "Upload")
-						OVERRIDE_DETAILED = false
-						cam.Start2D()
-						render.PushRenderTarget(StatsRT)
-						if (not pcall(show.PaintManual, show)) then
-							Derma_Message("Encountered an error while generating the image! Please try again.", "Upload failed", "Thanks")
-
-							render.Clear(0, 0, 0, 0)
-							render.PopRenderTarget(StatsRT)
-							cam.End2D()
-							show:Remove()
-						return end
-						local data = render.Capture {
-							format = "png",
-							quality = 100,
-							h = show:GetTall(),
-							w = show:GetWide(),
-							x = 0,
-							y = 0,
-							alpha = false,
-						}
-						render.Clear(0, 0, 0, 0)
-						render.PopRenderTarget(StatsRT)
-						cam.End2D()
-						show:Remove()
-						HTTP {
-							url = "https://api.imgur.com/3/album",
-							method = "post",
-							headers = {
-								Authorization = "Client-ID 3693fd6ea039830",
-							},
-							success = function(_, c, _, _)
-								local album = util.JSONToTable(c)
-								if (not album.success) then
-									Derma_Message("Your upload was not successful! Please try again.", "Upload failed", ":(")
-									return
-								end
-
-								HTTP {
-									url = "https://api.imgur.com/3/image",
-									method = "post",
-									headers = {
-										Authorization = "Client-ID 3693fd6ea039830"
-									},
-									success = function(_, b, _, _)
-										b = util.JSONToTable(b)
-										if (b.success) then
-											Derma_Message("Your picture of your stats has been uploaded and copied to your clipboard!\nYou can now simply paste it anywhere, like our Discord! discord.gg/pluto", "Upload success", "Thanks!")
-											SetClipboardText("https://imgur.com/a/" .. album.data.id)
-										else
-											Derma_Message("Your upload was not successful! Please try again.", "Upload failed", "Thanks")
-										end
-									end,
-									failed = function(a) 
-										Derma_Message("Imgur appears to be having some issues, please wait and try again!", "Upload failed", "Ok")
-									end,
-									parameters = {
-										image = util.Base64Encode(data),
-										album = album.data.deletehash
-									},
-								}
-							end,
-							failed = function(a)
-								Derma_Message("Your upload was not successful! Please try again.", "Upload failed", "Ok")
-							end,
-							parameters = {
-								title = string.format("%s's %s", LocalPlayer():Nick(), item_name),
-								description = string.format("Taken by %s https://steamcommunity.com/profiles/%s\nDiscord: https://discord.gg/pluto\nWebsite: https://pluto.gg\n\n%s", LocalPlayer():Nick(), LocalPlayer():SteamID64(), self.Item:GetTextMessage()), 
-							},
-						}
-					end)
-				end)
-			end):SetIcon("icon16/camera.png")
-
-			if (self.Item.Type ~= "Shard") then
-				rightclick_menu:AddOption("Toggle locked", function()
-					pluto.inv.message()
-						:write("itemlock", self.Item.ID)
-						:send()
-				end):SetIcon("icon16/lock.png")
-			end
-
-			if (not self.Item.Locked and self.Item.Nickname) then
-				rightclick_menu:AddOption("Remove name (100 hands)", function()
-					self.Item.Nickname = nil
-					pluto.inv.message()
-						:write("unname", self.Item.ID)
-						:send()
-				end):SetIcon("icon16/cog_delete.png")
-			end
-
-			if (LocalPlayer():GetUserGroup() == "developer") then
-				rightclick_menu:AddOption("Duplicate", function()
-					RunConsoleCommand("pluto_item_dupe", self.Item.ID)
-				end):SetIcon("icon16/cog_delete.png")
-			end
-
-			rightclick_menu:Open()
-			rightclick_menu:SetPos(input.GetCursorPos())--s
+			CreateMenu(self, self.Item)
 		end
 	end
 end
