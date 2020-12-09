fc940e3072c7e401b607da9b33740ba768f43b0d
Meepen <meepdarknessmeep@gmail.com>
2020-12-09 09:56:54 -0500

    guns with id below 2million will have 15% effectiveness with coined but will have the old way of applying coined

--- lua/pluto/mods/modifiers/coined.lua
+++ lua/pluto/mods/modifiers/coined.lua
@@ -18,6 +18,8 @@ end
 
 MOD.Description = "Gives 15% more currency rewards per max modifier"
 
+MOD.NoCoined = true
+
 MOD.Tiers = {
 	{ 1, 1 },
 }
@@ -25,8 +27,23 @@ MOD.Tiers = {
 function MOD:OnUpdateSpawnPoints(wep, rolls, atk, vic, state)
 	if (IsValid(atk) and state.Points > 0) then
 		local gun = wep.PlutoGun
+		if (not gun.RowID or gun.RowID < 2000000) then
+			state.Points = state.Points * (1 + 0.15 * gun:GetMaxAffixes())
+		else
+			local mod_count = 0
 
-		state.Points = state.Points * (1 + 0.15 * gun:GetMaxAffixes())
+			for _, modlist in pairs(gun.Mods) do
+				mod_count = mod_count + #modlist
+
+				for _, mod in pairs(modlist) do
+					if (pluto.mods.byname[mod.Mod].NoCoined) then
+						mod_count = mod_count - 1
+					end
+				end
+			end
+
+			state.Points = state.Points * (1 + 0.1 * mod_count)
+		end
 	end
 end
 
