ba4677ef3cbc322836d1acae3716395853c47c47
Meepen <meepdarknessmeep@gmail.com>
2020-12-09 09:51:24 -0500

    BALANCE: coined now goes off max modifiers instead of total modifiers

--- lua/pluto/mods/modifiers/coined.lua
+++ lua/pluto/mods/modifiers/coined.lua
@@ -16,9 +16,7 @@ function MOD:FormatModifier(index, roll)
 	return ""
 end
 
-MOD.Description = "Gives 12% more currency rewards per modifier"
-
-MOD.NoCoined = true
+MOD.Description = "Gives 15% more currency rewards per max modifier"
 
 MOD.Tiers = {
 	{ 1, 1 },
@@ -27,19 +25,8 @@ MOD.Tiers = {
 function MOD:OnUpdateSpawnPoints(wep, rolls, atk, vic, state)
 	if (IsValid(atk) and state.Points > 0) then
 		local gun = wep.PlutoGun
-		local mod_count = 0
 
-		for _, modlist in pairs(gun.Mods) do
-			mod_count = mod_count + #modlist
-
-			for _, mod in pairs(modlist) do
-				if (pluto.mods.byname[mod.Mod].NoCoined) then
-					mod_count = mod_count - 1
-				end
-			end
-		end
-
-		state.Points = state.Points * (1 + 0.12 * mod_count)
+		state.Points = state.Points * (1 + 0.15 * gun:GetMaxAffixes())
 	end
 end
 
