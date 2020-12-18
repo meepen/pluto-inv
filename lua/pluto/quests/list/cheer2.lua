ec096843ce08868a8175b80e1c5de3deeb935509
itsmeaddof123 <itsmeadd.123@gmail.com>
2020-12-17 23:50:05 -0600

    bells jingle in the quest tab

--- lua/pluto/quests/list/cheer2.lua
+++ lua/pluto/quests/list/cheer2.lua
@@ -5,7 +5,7 @@ QUEST.Color = Color(153, 25, 0)
 QUEST.RewardPool = "unique"
 
 function QUEST:GetRewardText()
-	return "Dart Pistol"
+	return "Snowball Shooter"
 end
 
 function QUEST:Init(data)
@@ -16,12 +16,7 @@ end
 
 function QUEST:Reward(data)
 	pluto.db.transact(function(db)
-		local new_item = pluto.inv.generatebufferweapon(db, data.Player, "unique", "tfa_cso_dartpistol")
-		new_item.CreationMethod = "QUEST"
-		if (not new_item) then
-			mysql_rollback(db)
-			return
-		end
+		local new_item = pluto.inv.generatebufferweapon(db, data.Player, "QUEST", "unique", "tfa_cso_mg36_xmas")
 		mysql_commit(db)
 
 		data.Player:ChatPrint(white_text, "You have received ", startswithvowel(new_item.Tier.Name) and "an " or "a ", new_item, white_text, " for completing ", self.Color, self.Name, white_text, "!")
