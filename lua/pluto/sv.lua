cdf94dad9a5c007e8d02efcf7f605f44c3109b6e
itsmeaddof123 <itsmeadd.123@gmail.com>
2020-12-18 18:45:21 -0600

    Added a meme event

--- lua/pluto/sv.lua
+++ lua/pluto/sv.lua
@@ -42,6 +42,7 @@ pluto.files.load {
 		"events/minis/sv_raining.lua",
 		"events/minis/sv_dice.lua",
 		"events/minis/sv_stars.lua",
+		"events/minis/sv_dash.lua",
 		"events/sv_aprilfools.lua",
 
 		"chat/sv_chat.lua",
