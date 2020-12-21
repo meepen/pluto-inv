86733c29c653b78cf1b011bf75fb3acd80dacba0
Meepen <meepdarknessmeep@gmail.com>
2020-12-20 19:08:36 -0500

    fix damage node

--- lua/pluto/inv/nodes/list/stats/damage.lua
+++ lua/pluto/inv/nodes/list/stats/damage.lua
@@ -4,10 +4,10 @@ NODE.Name = "Strength Increase"
 NODE.Experience = 2650
 
 function NODE:GetDescription(node)
-	return string.format("Damage is increased by %.2f%%", 3 + node.node_val1 * 5)
+	return string.format("Damage is increased by %.2f%%", 3 + node.node_val1 * 2)
 end
 
 function NODE:ModifyWeapon(node, wep)
 	wep:DefinePlutoOverrides "Damage"
-	wep.Pluto.Damage = wep.Pluto.Damage + wep:ScaleRollType("damage", node.node_val1 / 100 * 5, true)
+	wep.Pluto.Damage = wep.Pluto.Damage + wep:ScaleRollType("damage", (3 + node.node_val1 * 2) / 100, true)
 end
