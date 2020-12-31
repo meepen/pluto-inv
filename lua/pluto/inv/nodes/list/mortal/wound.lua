e6822f775afdaa6720f70b65eabd92d957044316
Meepen <meepdarknessmeep@gmail.com>
2020-12-31 13:27:14 -0500

    buff mortal and minor wound (35% and 10%)

--- lua/pluto/inv/nodes/list/mortal/wound.lua
+++ lua/pluto/inv/nodes/list/mortal/wound.lua
@@ -1,11 +1,11 @@
 local NODE = pluto.nodes.get "mortal_wound"
 
 NODE.Name = "Mortal Wound"
-NODE.Description = "Damage with this gun removes 20% of the damage's max health from enemy."
+NODE.Description = "Damage with this gun removes 35% of the damage's max health from enemy."
 NODE.Experience = 5750
 
 function NODE:ModifyWeapon(node, wep)
-	wep.MortalWound = (wep.MortalWound or 0) + 0.2
+	wep.MortalWound = (wep.MortalWound or 0) + 0.35
 	local id = "pluto_mortal_wound_" .. wep:GetPlutoID()
 
 	hook.Add("PlutoPreDamage", id, function(shooter, vic, dmg)
