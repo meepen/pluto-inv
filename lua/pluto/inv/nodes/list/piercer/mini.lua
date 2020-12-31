e1439535600bf95f0f2f800ad37fca9b002e5492
Meepen <meepdarknessmeep@gmail.com>
2020-12-31 13:31:22 -0500

    wall piercer and mini wall piercer now show effectiveness (and are buffed from 30->45 and 10->15)

--- lua/pluto/inv/nodes/list/piercer/mini.lua
+++ lua/pluto/inv/nodes/list/piercer/mini.lua
@@ -2,8 +2,8 @@ local NODE = pluto.nodes.get "pierce_mini"
 
 NODE.Name = "Mini Wall Piercer"
 NODE.Experience = 4300
-NODE.Description = "Your bullets pierce walls more effectively."
+NODE.Description = "Your bullets pierce walls with 15 more power."
 
 function NODE:ModifyWeapon(node, wep)
-	wep.Primary.PenetrationValue = (wep.Primary.PenetrationValue or 0) + 10
+	wep.Primary.PenetrationValue = (wep.Primary.PenetrationValue or 0) + 15
 end
--- lua/pluto/inv/nodes/list/piercer/pierce.lua
+++ lua/pluto/inv/nodes/list/piercer/pierce.lua
@@ -2,8 +2,8 @@ local NODE = pluto.nodes.get "pierce_pierce"
 
 NODE.Name = "Wall Piercer"
 NODE.Experience = 7300
-NODE.Description = "Your bullets pierce walls."
+NODE.Description = "Your bullets pierce walls with 40 power."
 
 function NODE:ModifyWeapon(node, wep)
-	wep.Primary.PenetrationValue = (wep.Primary.PenetrationValue or 0) + 30
+	wep.Primary.PenetrationValue = (wep.Primary.PenetrationValue or 0) + 40
 end
