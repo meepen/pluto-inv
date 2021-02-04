1a35f7451c948f58d8b1ceb8f9353d2c9f65cf5f
Meepen <meepdarknessmeep@gmail.com>
2021-02-04 13:46:48 -0500

    lighten up unique purple color

--- lua/pluto/tiers/unique.lua
+++ lua/pluto/tiers/unique.lua
@@ -3,7 +3,7 @@ return {
 	affixes = 0,
 	SubDescription = "This item is unique. It cannot roll mods",
 	Shares = 0,
-	Color = Color(59, 30, 117),
+	Color = Color(94, 59, 163),
 	NoChange = true,
 	CraftChance = 0,
 }
