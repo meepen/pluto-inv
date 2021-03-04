995eccdd84cd68dc0a95dfdb92db93a987a9c1aa
Meepen <meepdarknessmeep@gmail.com>
2021-03-04 04:46:54 -0500

    update currency selector (FIRST TRY LETS GO)

--- lua/pluto/cl/vgui/inv/parts/tradingset.lua
+++ lua/pluto/cl/vgui/inv/parts/tradingset.lua
@@ -31,12 +31,16 @@ function PANEL:Init()
 
 	self.CurrencySelectors = {}
 
-	for i = 1, 3 do
+	for slot = 1, 3 do
 		local selector = self.Inner:Add "pluto_inventory_trading_set_currency_selector"
-		self.CurrencySelectors[i] = selector
+		self.CurrencySelectors[slot] = selector
 		selector:Dock(BOTTOM)
 		selector:SetTall(self.CurrencySize)
-		selector:DockMargin(self.Padding, self.Padding, self.Padding, i == 1 and self.Padding or 0)
+		selector:DockMargin(self.Padding, self.Padding, self.Padding, slot == 1 and self.Padding or 0)
+
+		function selector.OnCurrencyUpdated(s)
+			self:OnCurrencyUpdated(slot, s:GetCurrency(), s:GetAmount())
+		end
 	end
 
 	self.ItemLines = {}
@@ -106,7 +110,6 @@ function PANEL:OnItemUpdated(slot, item)
 end
 
 function PANEL:OnCurrencyUpdated(slot, currency, amount)
-	ErrorNoHalt "OnCurrencyUpdated not overridden"
 end
 
 function PANEL:SetText(text)
@@ -171,7 +174,7 @@ vgui.Register("pluto_inventory_trading_set", PANEL, "EditablePanel")
 
 local PANEL = {}
 AccessorFunc(PANEL, "Currency", "Currency")
-AccessorFunc(PANEL, "Amount", "Amount")
+AccessorFunc(PANEL, "CurrencyAmount", "Amount")
 
 function PANEL:Init()
 	self.Inner = self:Add "ttt_curved_panel"
@@ -183,22 +186,110 @@ function PANEL:Init()
 		self:SetWide(h)
 	end
 	self.CurrencyIcon:SetCursor "hand"
-	function self.CurrencyIcon:OnMousePressed(m)
+	function self.CurrencyIcon.OnMousePressed(s, m)
 		if (m == MOUSE_LEFT) then
-			-- select currency then change back
+			pluto.ui.currencyselect("Select a currency to trade.",
+				function(cur)
+					if (not IsValid(self)) then
+						return
+					end
+
+					self:SetCurrency(cur)
+				end,
+				function(cur)
+					return (pluto.cl_currency[cur.InternalName] or 0) > 0
+				end
+			)
+		elseif (m == MOUSE_RIGHT) then
+			self:Reset()
+		end
+	end
+
+	self.Amount = self.Inner:Add "pluto_label"
+	self.Amount:Dock(FILL)
+	self.Amount:SetRenderSystem(pluto.fonts.systems.shadow)
+	self.Amount:SetFont "pluto_inventory_font"
+	self.Amount:SetTextColor(Color(255, 255, 255))
+	self.Amount:SetText "hi"
+	self.Amount:SetContentAlignment(6)
+	self.Amount:SetCursor "beam"
+	self.Amount:SetMouseInputEnabled(true)
+	self.Amount:DockMargin(3, 0, 3, 0)
+
+	function self.Amount.OnMousePressed(s)
+		if (not self:GetCurrency()) then
+			return
+		end
+
+		local t = s:GetText()
+		s:SetText ""
+		local input = s:Add "DTextEntry"
+		input:SetTextColor(s:GetTextColor())
+		input:SetFont(s:GetFont())
+		input:Dock(FILL)
+		pluto.ui.pnl:AddKeyboardFocus(1)
+		input:RequestFocus()
+		input:SetUpdateOnType(true)
+
+		local function finish()
+			local num = tonumber(input:GetText() or 1)
+			self:SetAmount(num)
+			input:Remove()
+		end
+
+		function input.OnEnter()
+			finish()
+		end
+
+		function input.OnFocusChanged(gained)
+			if (not gained) then
+				finish()
+			end
+		end
+
+		function input.OnRemove()
+			pluto.ui.pnl:AddKeyboardFocus(-1)
 		end
 	end
 
 	self.CurrencyIcon:Dock(LEFT)
 
-	self.CurrencyIcon:SetImage "pluto/currencies/questionmark.png"
 	self.CurrencyIcon:DockMargin(3, 0, 3, 0)
 
 	self:SetColor(Color(95, 96, 102))
 	self:SetCurve(2)
 	self.Inner:SetCurve(2)
 	self.Inner:SetColor(Color(53, 53, 60))
+
+	self:Reset()
 end
 
+function PANEL:SetCurrency(cur)
+	self.Currency = cur
+
+	if (cur) then
+		self:SetAmount(1)
+		self.CurrencyIcon:SetImage(cur.Icon)
+	else
+		self:Reset()
+	end
+
+	self:OnCurrencyUpdated()
+end
+
+function PANEL:SetAmount(amount)
+	self.CurrencyAmount = amount or 0
+	self.Amount:SetText(amount or "")
+	self:OnCurrencyUpdated()
+end
+
+function PANEL:OnCurrencyUpdated()
+end
+
+function PANEL:Reset()
+	self.Currency = nil
+	self.CurrencyIcon:SetImage "pluto/currencies/questionmark.png"
+	self:SetAmount()
+end
 
 vgui.Register("pluto_inventory_trading_set_currency_selector", PANEL, "ttt_curved_panel_outline")
