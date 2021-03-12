c7056fe8069a45a39fe679eeeb8f9950ecdafe36
Meepen <meepdarknessmeep@gmail.com>
2021-03-12 13:42:49 -0500

    change inevitable background

--- lua/pluto/tiers/inevitable.lua
+++ lua/pluto/tiers/inevitable.lua
@@ -1,4 +1,20 @@
-local diff = 0.1
+
+
+local aw, ah = 16, 16
+local speed = 1.5
+local function DrawArrow(x, y, w, h)
+	surface.DrawPoly {
+		{ x = x, y = y },
+		{ x = x + w, y = y + h * 0.5 },
+		{ x = x + w * 0.2, y = y + h * 0.5 },
+	}
+	surface.DrawPoly {
+		{ x = x + w, y = y + h * 0.5 },
+		{ x = x, y = y + h },
+		{ x = x + w * 0.2, y = y + h * 0.5 },
+	}
+end
+
 return {
 	Name = "Inevitable",
 	affixes = 3,
@@ -6,12 +22,14 @@ return {
 		firerate = 1,
 	},
 	guaranteeddraw = function(x, y, sx, sy, w, h, rand)
-		math.randomseed(math.floor((CurTime() + rand) / diff))
+		surface.SetDrawColor(0, 0, 255, 128)
+		draw.NoTexture()
 
-		surface.SetDrawColor(0, 255, 255)
-		for i = 1, 3 do
-			surface.DrawLine(math.random() * w + x, y, x + math.random() * w, y + h)
-		end
+		DrawArrow(x + (CurTime() % speed) / speed * w, y + h * 0.1, aw, ah)
+		DrawArrow(x - w + (CurTime() % speed) / speed * w, y + h * 0.1, aw, ah)
+
+		DrawArrow(x + ((speed / 2 + CurTime()) % speed) / speed * w, y + h - ah - h * 0.1, aw, ah)
+		DrawArrow(x - w + ((speed / 2 + CurTime()) % speed) / speed * w, y + h - ah - h * 0.1, aw, ah)
 	end,
 	SubDescription = {
 		guaranteed = "This always rolls RPM I",
