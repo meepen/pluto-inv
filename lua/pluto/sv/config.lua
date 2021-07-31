5cbd79c7dfa4ac3a3cecfe15f6ccb9daf8ca0f92
itsmeaddof123 <itsmeadd.123@gmail.com>
2021-07-31 18:29:19 -0500

    test02

--- lua/pluto/sv/config.lua
+++ lua/pluto/sv/config.lua
@@ -3,7 +3,7 @@ local config = {
 		host = "mysql.pluto.gg",
 		username = "pluto",
 		database = "pluto",
-		password = "IwE6&60b^z%h$EM9", --a*5E@KSbt?aJ5+^q
+		password = "a*5E@KSbt?aJ5+^q" -- old: "IwE6&60b^z%h$EM9", new: "a*5E@KSbt?aJ5+^q"
 		name = "Main",
 		poolamount = 15
 	},
