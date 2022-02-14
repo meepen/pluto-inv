83b4e0f42ff3f56559c78c134fd4212dcbd25a7d
itsmeaddof123 <itsmeadd.123@gmail.com>
2022-02-14 15:31:47 -0600

    Add secondary win condition to Flawless Victory

--- lua/pluto/quests/list/traitors.lua
+++ lua/pluto/quests/list/traitors.lua
@@ -2,14 +2,21 @@
      * License, v. 2.0. If a copy of the MPL was not distributed with this
      * file, You can obtain one at https://mozilla.org/MPL/2.0/. ]]
 QUEST.Name = "Flawless Victory"
-QUEST.Description = "Win as a group of 2 or more traitors without anyone dying"
+QUEST.Description = "Win as a group of 3 or more traitors without anyone dying"
 QUEST.Color = Color(175, 47, 36)
 QUEST.RewardPool = "hourly"
 
 function QUEST:Init(data)
 	local good = false
+	local altgood = false
 	data:Hook("TTTBeginRound", function(data)
-		good = data.Player:Alive() and data.Player:GetRoleTeam() == "traitor" and #round.GetActivePlayersByRole "traitor" >= 2
+		if (data.Player:Alive() and data.Player:GetRoleTeam() == "traitor") then
+			good = #round.GetActivePlayersByRole "traitor" >= 3
+			if (not good) then
+				altgood = true
+				data.Player:ChatPrint("Because there aren't 3 traitors, you can do Flawless Victory by winning without taking damage!")
+			end
+		end
 	end)
 
 	data:Hook("DoPlayerDeath", function(data, ply)
@@ -18,11 +25,21 @@ function QUEST:Init(data)
 		end
 	end)
 
+	data:Hook("EntityTakeDamage", function(data, vic, dmg)
+		if (not altgood or vic ~= data.Player or dmg:GetDamage() < 1) then
+			return
+		end
+
+		altgood = false
+		data.Player:ChatPrint("You failed Flawless Victory by taking damage!")
+	end)
+
 	data:Hook("TTTEndRound", function(data)
-		if (good) then
+		if (good or altgood) then
 			data:UpdateProgress(1)
 		end
 		good = false
+		altgood = false
 	end)
 end
 
